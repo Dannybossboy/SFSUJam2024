d37410ef93c49d20336ca64f57413d64edf9f1ca
LePuncake321 <lucky713ph@yahoo.com>
2024-09-21 22:44:29 -0700

    Objects can now interact with buttons
    Yay

--- Assets/_Scripts/Objects/WorldButton.cs
+++ Assets/_Scripts/Objects/WorldButton.cs
@@ -5,7 +5,22 @@ using UnityEngine.Events;
 
 public class WorldButton : MonoBehaviour
 {
-    public UnityEvent pressEvent;
+    public UnityEvent pressEvent; 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Player" || collision.collider.tag == "Obstacle")
+        {
+            print("It's a hit!");
+        }   
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Player" || collision.collider.tag == "Obstacle")
+        {
+            print("Goodbye!");
+        }
+    }
 
 }
